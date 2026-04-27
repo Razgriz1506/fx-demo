0edafc9a37a0aca2ec75c1ceb6770af8b3186fb0
Razgriz1506 <asifmahmud1506@gmail.com>
2026-04-27 23:01:03 +0600

    Update create-fx.dto.ts

--- backend/src/fx/create-fx.dto.ts
+++ backend/src/fx/create-fx.dto.ts
@@ -4,10 +4,4 @@ export class CreateFxDto {
   currency: string;
   amount: number;
   purpose: string;
-}export class CreateFxDto {
-  customer_name: string;
-  account_number: string;
-  currency: string;
-  amount: number;
-  purpose: string;
 }
