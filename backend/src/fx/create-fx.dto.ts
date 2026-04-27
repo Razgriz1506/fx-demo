99c7aa23ead80d77bf1611a6ef2cf9e156cb2007
Razgriz1506 <asifmahmud1506@gmail.com>
2026-04-27 22:53:58 +0600

    Update and rename create-fx-request.dto.ts to create-fx.dto.ts

---
+++ backend/src/fx/create-fx.dto.ts
@@ -0,0 +1,13 @@
+export class CreateFxDto {
+  customer_name: string;
+  account_number: string;
+  currency: string;
+  amount: number;
+  purpose: string;
+}export class CreateFxDto {
+  customer_name: string;
+  account_number: string;
+  currency: string;
+  amount: number;
+  purpose: string;
+}
